8c9172c4bc3e906ee99bedd099fa5050fda14b6d
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-03-15 01:51:56 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -10,7 +10,7 @@ text.scan(/\A[un][a-z]?\z[ing]/)
 end
 
 def words_five_letters_long(text)
-text.scan(/[a-z][5]/)
+text.scan(/\w[a-z][5]/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
