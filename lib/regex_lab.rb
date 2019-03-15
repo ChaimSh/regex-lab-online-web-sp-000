dbf58222992e7dd595e490af2e5b678c46e8132d
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-03-15 02:47:54 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -10,7 +10,7 @@ text.scan(/un[a-z]+ing/)
 end
 
 def words_five_letters_long(text)
-text.scan(/[a-zA-Z]{5}/)
+text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
